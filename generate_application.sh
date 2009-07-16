66a7fb2042441f7bc23c18fd490f4163b769e414
nico <nico@nico.(none)>
2009-07-16 23:16:31 +0900

    Added stub of generation script

---
+++ generate_application.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+APPLICATION_NAME="Milan subway map"
+
+# Generate namespace based on application name TODO
+APPLICATION_ID="milan-subway-map"
+
+cp -r . ../${APPLICATION_ID}
+cd ../${APPLICATION_ID}
+
+# TODO replace Java namespace in files
+
+# TODO explain in details how to generate the signed APK
